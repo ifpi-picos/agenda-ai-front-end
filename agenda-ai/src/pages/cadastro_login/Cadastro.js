4ff191de4a6485a4dfd0fb8e4878414cbb5dc0a4
Paulo André dos Reis Passos <pauloreispassos988@gmail.com>
2023-10-05 20:01:39 -0300

    Integração de cadastro com a API

--- agenda-ai/src/pages/cadastro_login/Cadastro.js
+++ agenda-ai/src/pages/cadastro_login/Cadastro.js
@@ -2,64 +2,47 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Cadastro.module.css'
 import { useState, useEffect } from 'react'
 import { faEye } from '@fortawesome/free-solid-svg-icons';
+
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+const schema = yup.object().shape({
+    nome: yup.string().required('O nome deve vser preenchido'),
+    email: yup.string().required('O email deve ser preenchido'),
+    password: yup.string().required('A senha deve ser preenchida')
+})
 
 function Cadastro() {
-    const [estados, setEstados] = useState([])
-    const [cidades, setCidades] = useState([])
-    const [estadoSelecionado, setEstadoSelecionado] = useState('');
-    const [cidadeSelecionada, setCidadeSelecionada] = useState('');
-    const navigate = useNavigate()
+    let navigate = useNavigate()
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        navigate('/Login')
-    }
+    const { register, handleSubmit, formState: {errors} } = useForm({
+        resolver: yupResolver(schema)
+    })
 
-    useEffect(() => {
-        fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-            .then(response => response.json())
-            .then(data => {
-                const estadosOrdenados = data.sort((a, b) => a.nome.localeCompare(b.nome));
-                setEstados(estadosOrdenados);
-            })
-            .catch(error => {
-                console.error('Erro ao buscar estados:', error);
-            });
-    }, []);
-
-    const handleEstadoChange = (event) => {
-        const estadoId = event.target.value;
-        setEstadoSelecionado(estadoId);
-        setCidadeSelecionada('');
-
-        fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoId}/municipios`)
-            .then(response => response.json())
-            .then(data => {
-                setCidades(data);
-            })
-            .catch(error => {
-                console.error('Erro ao buscar cidades:', error);
-            });
-    };
-
-    const handleCidadeChange = (event) => {
-        const cidadeId = event.target.value;
-        setCidadeSelecionada(cidadeId);
-    };
-
+    const onSubmit = (data) => axios.post("http://localhost:3001/auth/signup", {
+        nome: data.nome,
+        email: data.email,
+        password: data.password,
+        tipo: 'cliente'
+    }).then((response) => {
+        navigate("/Home")
+    })
 
     return (
         <section className={styles.cadastro}>
             <div className={styles.cardCadastro}>
                 <h1>Cadastro</h1>
-                <form onSubmit={handleSubmit} className={styles.formCadastro}>
+                <form onSubmit={handleSubmit(onSubmit)} className={styles.formCadastro}>
                     <input
                         type='text'
                         id='nome'
                         name='nome'
                         placeholder='Digite seu nome'
                         required
+                        {...register("nome")}
                     />
                     <input
                         type='text'
@@ -67,35 +50,17 @@ function Cadastro() {
                         name='email'
                         placeholder='Digite seu email'
                         required
+                        {...register("email")}
                     />
-
-                    <div className={styles.divSelect}>
-                        <label>Estado: </label>
-                        <select value={estadoSelecionado} onChange={handleEstadoChange}>
-                            <option value="">Selecione o estado que você mora</option>
-                            {estados.map(estado => (
-                                <option key={estado.id} value={estado.id}>{estado.nome}</option>
-                            ))}
-                        </select>
-                    </div>
-
-                    <div className={styles.divSelect}>
-                        <label>Cidade: </label>
-                        <select value={cidadeSelecionada} onChange={handleCidadeChange} disabled={!estadoSelecionado}>
-                            <option value="">Selecione a cidade que você mora</option>
-                            {cidades.map(cidade => (
-                                <option key={cidade.id} value={cidade.id}>{cidade.nome}</option>
-                            ))}
-                        </select>
-                    </div>
 
                     <div className={styles.inputSenha}>
                         <input
                             type='password'
-                            id='senha'
-                            name='senha'
+                            id='password'
+                            name='password'
                             placeholder='Digite sua senha'
                             required
+                            {...register("password")}
                         />
                         <FontAwesomeIcon className={styles.icon} icon={faEye}/>
                     </div>
