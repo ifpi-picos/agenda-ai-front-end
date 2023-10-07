f61560f87a1472169afa0fa6cc3d31dc7b0e1a7a
Paulo André dos Reis Passos <pauloreispassos988@gmail.com>
2023-10-07 12:21:29 -0300

    update auth\signin

--- app/src/app/auth/signin/page.js
+++ app/src/app/auth/signin/page.js
@@ -5,26 +5,48 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
 import Navbar from '@/components/Navbar'
 import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
+
+
 
 export default function Signin() {
     const router = useRouter();
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        router.push('/')
-    }
+    const { register, handleSubmit, formState: {errors}} = useForm({
+
+    })
+    const onSubmit = (data) => axios.post("http://localhost:3001/auth/signin", {
+        email: data.email,
+        password: data.password
+    }).then((response) => {
+        localStorage.setItem('token', response.data.token);
+        router.push('/home')
+    })
     return (
         <>
             <Navbar />
             <div className={styles.login}>
                 <div className={styles.cardLogin}>
                     <h1>Login</h1>
-                    <form className={styles.formLogin} onSubmit={handleSubmit}>
+                    <form className={styles.formLogin} onSubmit={handleSubmit(onSubmit)}>
                         <div>
-                            <input id="login" type="email" placeholder="Email" required />
+                            <input 
+                                id="login"
+                                type="email"
+                                placeholder="Email"
+                                required
+                                {...register("email")}
+                            />
                         </div>
                         <div className={styles.inputDiv}>
-                            <input type="password" id="password" placeholder="Senha" required />
+                            <input 
+                                type="password"
+                                id="password"
+                                placeholder="Senha"
+                                required
+                                {...register('password')}
+                            />
                             <FontAwesomeIcon className={styles.icon} icon={faEye}/>
                         </div>
                         <button id='buttonLogin' className={styles.submitButton} type='submit'>Entrar</button>
