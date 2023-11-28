fa49122a33ea5b22912ea274d615d219e44ed178
Paulo André dos Reis Passos <pauloreispassos988@gmail.com>
2023-11-28 12:37:54 -0300

    modal de confirmação de remoção de lanche

--- src/components/Lanchonetes/VerLanche.js
+++ src/components/Lanchonetes/VerLanche.js
@@ -1,16 +1,39 @@
 import Lanche from '@/services/Lanche';
 import styles from '@/app/lanche/lanche.module.css'
+import stylesModal from '@/components/layout/SucessErrorModal.module.css'
 import Image from 'next/image'
+import { useEffect, useState } from 'react';
 
 function VerLanche({ lanche, editMode, setEditMode }) {
-    
+    const [token, setToken] = useState('')
+    const [showDeleteModal, setShowDeleteModal]= useState(false)
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem("token")
+        if (storedToken) {
+            setToken(storedToken)
+        }
+    }, [])
+
     const handleEditClick = () => {
         console.log('setEditMode(true);')
         setEditMode(true);
     };
-    const handleRemoveClick = async () => {
-        await Lanche.deletarPorId(lanche.idLanche)
-        redirecionar()
+    const handleRemoveClick = () => {
+        setShowDeleteModal(true)
+        /*if (token) {
+            await Lanche.deletarPorId(lanche.idLanche, token)
+        }*/
+        //redirecionar()
+    }
+    const onClose = () => {
+        setShowDeleteModal(false)
+    }
+    const confirmarRemoverLanche = async () => {
+        if (token) {
+            await Lanche.deletarPorId(lanche.idLanche, token)
+            redirecionar()
+        }
     }
     const redirecionar = () => {
         window.location.href = (`/gerente/${lanche.idLanchonete}/lanches`)
@@ -34,6 +57,17 @@ function VerLanche({ lanche, editMode, setEditMode }) {
                     <button onClick={handleRemoveClick}>Remover</button>
                     <button onClick={handleEditClick}>Editar</button>
                 </div>
+                {showDeleteModal && (
+                    <div className={styles.modal}>
+                    <div className={styles.modal_content}>
+                      <p>Realmente deseja remover esse lanche de sua lanchonete?</p>
+                      <div className={styles.botoesModal}>
+                          <button onClick={onClose}>Cancelar</button>
+                          <button onClick={confirmarRemoverLanche}>Confirmar</button>
+                      </div>
+                    </div>
+                  </div>
+                )}
             </div>
         </div>
     )
