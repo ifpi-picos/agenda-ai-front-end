a3a12b0a98635c459be3242dcf67b30d2530eb5a
Paulo André dos Reis Passos <pauloreispassos988@gmail.com>
2024-01-03 14:06:39 -0300

    Envio de código de confirmação de cadastro

--- src/app/auth/signup/page.js
+++ src/app/auth/signup/page.js
@@ -15,6 +15,7 @@ import styles from '@/app/auth/signup/signup.module.css'
 import { useState } from "react";
 import { apiUrl } from "@/config/config";
 import Modal from "@/components/layout/SucessErrorModal";
+import Loading from "@/components/Loading";
 
 
 const schema = yup.object().shape({
@@ -27,9 +28,14 @@ const schema = yup.object().shape({
 export default function useSignup() {
     const router = useRouter()
 
+    const [confirmModal, setConfirmModal] = useState(false)
+    const [loading, setLoading] = useState(false)
+
     const [errorMessage, setErrorMessage] = useState(null)
     const [successMessage, setSuccessMessage] = useState(null);
-    const [loading, setLoading] = useState(null)
+
+    const [confirmationCode, setConfirmationCode] = useState('')
+
 
     const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(schema)
@@ -41,11 +47,13 @@ export default function useSignup() {
             nomeUsuario: data.nomeUsuario,
             email: data.email,
             password: data.password,
-            confirmPassword: data.confirmPassword,
-            tipo: 'cliente'
+            confirmPassword: data.confirmPassword
         }).then((response) => {
-            router.push('/auth/signin')
+            //router.push('/auth/signin')
+            setLoading(false)
+            setConfirmModal(true)
         }).catch((error) => {
+            setLoading(false)
             if (error.response) {
                 const responseData = error.response.data;
                 if (responseData.error) {
@@ -66,71 +74,140 @@ export default function useSignup() {
         }
     };
 
+    const onSubmitConfirm = (data) => {
+        setLoading(true)
+        axios.post(`${apiUrl}/auth/confirmSignup`, {
+            cod: confirmationCode,
+            nomeUsuario: data.nomeUsuario,
+            email: data.email,
+            password: data.password,
+        })
+            .then((response) => {
+                setErrorMessage(null)
+                setLoading(false);
+                setConfirmModal(false);
+                setSuccessMessage("Cadastro confirmado com sucesso!");
+                router.push('/auth/signin')
+            })
+
+            .catch((error) => {
+                setLoading(false);
+                if (error.response) {
+                    const responseData = error.response.data;
+                    if (responseData.error) {
+                        setErrorMessage(responseData.error);
+                    }
+                }
+            });
+    }
+
     return (
         <>
             <Navbar />
-            <section className={styles.cadastro}>
-                <div className={styles.cardCadastro}>
-                    <h1>Cadastro</h1>
-                    <form onSubmit={handleSubmit(onSubmit)} className={styles.formCadastro}>
-                        <input
-                            type='text'
-                            id='nomeUsuario'
-                            name='nomeUsuario'
-                            placeholder='Digite seu nome'
-                            required
-                            {...register("nomeUsuario")}
-                        />
-                        <input
-                            type='text'
-                            id='email'
-                            name='email'
-                            placeholder='Digite seu email'
-                            required
-                            {...register("email")}
-                        />
-
-                        <div className={styles.inputSenha}>
-                            <input
-                                type={showPassword ? "text" : "password"}
-                                id='password'
-                                name='password'
-                                placeholder='Digite sua senha'
-                                required
-                                {...register("password")}
-                            />
-                            <FontAwesomeIcon className={styles.icon} icon={showPassword ? faEyeSlash : faEye} onClick={() => togglePasswordVisibility('password')} />
+            {loading ? (
+                <Loading />
+            ) : (
+                <section className={styles.cadastro}>
+                    {confirmModal ? (
+                        <div className={styles.cardCadastro}>
+                            <h1>Confirmar cadastro</h1>
+                            <form
+                                className={styles.formCadastro}
+                                onSubmit={handleSubmit(onSubmitConfirm)}>
+                                <p className={styles.text}>Um código de confirmação foi enviado para o seu email. Insira ele no campo abaixo:</p>
+                                <input
+                                    type="text"
+                                    id="confirmationCode"
+                                    name="confirmationCode"
+                                    maxLength={6}
+                                    value={confirmationCode}
+                                    onChange={(e) => {
+                                        const numericValue = e.target.value.replace(/\D/g, '');
+                                        if (!isNaN(numericValue) && numericValue.length <= 6) {
+                                            setConfirmationCode(numericValue);
+                                        }
+                                    }}
+                                />
+                                {errorMessage &&
+                                    <p>{errorMessage}</p>}
+                                <button
+                                    id="buttonConfirm"
+                                    className={styles.submitButton}
+                                    type='submit'
+                                >
+                                    Enviar
+                                </button>
+                            </form>
                         </div>
+                    ) : (
+                        <div className={styles.cardCadastro}>
+                            <h1>Cadastro</h1>
+                            <form onSubmit={handleSubmit(onSubmit)} className={styles.formCadastro}>
+                                <input
+                                    type='text'
+                                    id='nomeUsuario'
+                                    name='nomeUsuario'
+                                    placeholder='Digite seu nome'
+                                    required
+                                    {...register("nomeUsuario")}
+                                />
+                                <input
+                                    type='text'
+                                    id='email'
+                                    name='email'
+                                    placeholder='Digite seu email'
+                                    required
+                                    {...register("email")}
+                                />
 
-                        <div className={styles.inputSenha}>
-                            <input
-                                type={showConfirmPassword ? 'text' : 'password'}
-                                id='confirmPassword'
-                                name='confirmPassword'
-                                placeholder='Confirme sua senha'
-                                required
-                                {...register("confirmPassword")}
-                            />
-                            <FontAwesomeIcon className={styles.icon} icon={showConfirmPassword ? faEyeSlash : faEye} onClick={() => togglePasswordVisibility('confirmPassword')} />
+                                <div className={styles.inputSenha}>
+                                    <input
+                                        type={showPassword ? "text" : "password"}
+                                        id='password'
+                                        name='password'
+                                        placeholder='Digite sua senha'
+                                        required
+                                        {...register("password")}
+                                    />
+                                    <FontAwesomeIcon className={styles.icon} icon={showPassword ? faEyeSlash : faEye} onClick={() => togglePasswordVisibility('password')} />
+                                </div>
+
+                                <div className={styles.inputSenha}>
+                                    <input
+                                        type={showConfirmPassword ? 'text' : 'password'}
+                                        id='confirmPassword'
+                                        name='confirmPassword'
+                                        placeholder='Confirme sua senha'
+                                        required
+                                        {...register("confirmPassword")}
+                                    />
+                                    <FontAwesomeIcon className={styles.icon} icon={showConfirmPassword ? faEyeSlash : faEye} onClick={() => togglePasswordVisibility('confirmPassword')} />
+                                </div>
+
+                                <button
+                                    id='buttonLogin'
+                                    className={styles.submitButton} type='submit'
+                                >
+                                    Cadastrar
+                                </button>
+
+                            </form>
+                            {errorMessage &&
+                                <Modal
+                                    isOpen={true}
+                                    onClose={() => setErrorMessage(null)}
+                                    message={errorMessage}
+                                />
+                            }
+
+                            <div className={styles.ref}>
+                                <span>Já possui uma conta?</span>
+                                <Link href='/auth/signin'>Login</Link>
+                            </div>
                         </div>
-
-                        <button id='buttonLogin' className={styles.submitButton} type='submit'>Cadastrar</button>
-
-                    </form>
-                    {errorMessage &&
-                        <Modal
-                            isOpen={true}
-                            onClose={() => setErrorMessage(null)}
-                            message={errorMessage}
-                        />
-                    }
-
-                    <div className={styles.ref}>
-                        <span>Já possui uma conta?</span>
-                        <Link href='/auth/signin'>Login</Link>
-                    </div>
-                </div>
-            </section>
+                    )}
+                </section>
+            )}
         </>
     )
 }
